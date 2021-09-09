f39b9774cbbf57ea9e7d5bccf3bf13c2a33454ea
Orbry <orbryak@gmail.com>
2021-09-09 20:36:10 +0300

    save load color
    implemeneted saving chosen color to persistent storage on game exit and loading it on game launch

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
     public Color TeamColor;
+    
+    private string saveFileName = "savefile.json";
+    private string saveFilePath;
 
     private void Awake()
     {
@@ -15,7 +19,36 @@ public class MainManager : MonoBehaviour
             return;
         }
         
+        saveFilePath = $"{Application.persistentDataPath}/{ saveFileName}";
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+    
+    [System.Serializable]
+    internal class SaveData
+    {
+        public Color TeamColor;
+    }
+    
+    public void SaveColor()
+    {
+        SaveData data = new SaveData() { TeamColor = TeamColor };
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(saveFilePath, json);
+    }
+    
+    public void LoadColor()
+    {
+        string path = saveFilePath;
+        
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.TeamColor;
+        }
     }
 }
